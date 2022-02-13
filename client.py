b65c92062106d450fbf0b4056fd80bf1f177c499
yakovlevbro <hamilioon@gmail.com>
2022-02-13 15:24:16 +0300

    добавил кнопки и нажатие на них

--- client.py
+++ client.py
@@ -9,6 +9,31 @@ win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Клиент")
 
 
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 100
+        self.height = 100
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 20)
+        text = font.render(self.text, True, (255, 255, 255))
+        win.blit(text, (self.x + round(self.width / 2) - round(text.get_width() / 2),
+                        self.y + round(self.height / 2) - round(text.get_height() / 2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
+
+
 def redraw_window(win, game, p):
     win.fill((255, 255, 255))
 
@@ -16,8 +41,15 @@ def redraw_window(win, game, p):
         font = pygame.font.SysFont("comicsans", 30)
         text = font.render("Ожидаем игроков...", True, (0, 0, 0))
         win.blit(text, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2))
+    else:
+        for btn in btns:
+            btn.draw(win)
 
     pygame.display.update()
+
+
+btns = [Button("Rock", 25, 350, (0, 0, 0)), Button("Scissors", 150, 350, (0, 0, 0)),
+        Button("Paper", 275, 350, (0, 0, 0))]
 
 
 def main():
@@ -54,6 +86,20 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for btn in btns:
+                    if btn.click(pos) and game.connected():
+                        if player == 0:
+                            if not game.p1_went:
+                                print(btn.text)
+                        elif player == 1:
+                            if not game.p2_went:
+                                print(btn.text)
+                        elif player == 2:
+                            if not game.p3_went:
+                                print(btn.text)
 
         redraw_window(win, game, player)
 
